bb83f5a1f0861a6669fa339cc3e78f7f799c8e22
rnct14 <rnct14@gmail.com>
2021-03-05 00:56:20 -0300

    Updates

---
+++ precache-manifest.d7c0e51e62ffdd687bf2e24f3f1696e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280a2b1fa6c924803f7e3d27af9f4ce3",
+    "url": "/meliapp/index.html"
+  },
+  {
+    "revision": "ece1610913ab290fb963",
+    "url": "/meliapp/static/css/main.99498a3c.chunk.css"
+  },
+  {
+    "revision": "de3659c75b5f4d58f41b",
+    "url": "/meliapp/static/js/2.058d66bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meliapp/static/js/2.058d66bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece1610913ab290fb963",
+    "url": "/meliapp/static/js/main.2dc1b58d.chunk.js"
+  },
+  {
+    "revision": "79e3e1cc2253b988a50f",
+    "url": "/meliapp/static/js/runtime-main.eca81268.js"
+  }
+]);
